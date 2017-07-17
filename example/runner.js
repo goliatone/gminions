64e2793806e2f2ec28a4d851081aabe16befe619
goliatone <burgosemi@gmail.com>
2017-07-17 00:45:14 -0400

    Clean example

--- example/runner.js
+++ example/runner.js
@@ -2,20 +2,25 @@
 const mqtt = require('mqtt');
 const Runner = require('../lib/commands/ProcessCommandRunner');
 
-let command = new Runner();
+let command = new Runner({
+    commandsPath: './example'
+});
+
 const client  = mqtt.connect('mqtt://test.mosquitto.org');
 
 client.on('connect', ()=>{
 
     function notify(event, status) {
         let message = JSON.stringify(event);
-        client.publish(`minion/${event.id}/${status}`, message);
+        let topic = `minion/${event.id}/${status}`;
+
+        console.log('Topic: %s', topic);
+
+        client.publish(topic, message);
         client.end();
     }
 
-    command.run({
-        commandsPath: './example'
-    }).then((result)=>{
+    command.run({}).then((result) => {
         notify(result, 'complete');
     }).catch((error)=>{
         notify(error, 'error');
