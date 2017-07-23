78fa93b8966b440fb9da4276f04b3a8f6b41731f
goliatone <burgosemi@gmail.com>
2017-07-23 16:55:12 -0400

    Add command bus example

---
+++ example/cbus.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const EventHub = require('../lib/events/EventBus');
+
+const Event = require('../lib/events/Event');
+const Command = require('../lib/commands/Command');
+const CommandBus = require('../lib/commands/CommandBus');
+const EventTransport = require('../lib/transports/emitter');
+const MqttTransport = require('../lib/transports/mqtt');
+const TransportManager = require('../lib/transports/transportManager');
+
+class AppRunCommand extends Command{
+    execute(event){
+        console.log('AppRunCommand: %j', event);
+    }
+}
+
+class PostRunCommand extends Command{
+    execute(event){
+        console.log('PostRunCommand: %j', event);
+    }
+}
+
+let dis = new EventHub({
+    transport: new TransportManager({
+        defaultTransport: 'mqtt'
+    }),
+    context: {
+        name: 'TestApp'
+    }
+});
+
+let command = new CommandBus({
+    eventBus: dis
+});
+
+command.add('app.run', AppRunCommand);
+command.add('app.run.post', PostRunCommand);
+
+dis.subscribe({
+    once: true,
+    type: 'app.+.post',
+    // transport: ['mqtt'],
+}, (event)=>{
+    console.log('† Only once subscriber: %j', event);
+    command.execute(event);
+});
+
+dis.subscribe('app.*', (event)=>{
+    console.log('√ subscriber: %j', event);
+    command.execute(event);
+});
