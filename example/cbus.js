428e3a722c6f74eed5c078cc5f239413204e428e
goliatone <burgosemi@gmail.com>
2017-07-25 09:35:27 -0400

    Update example

--- example/cbus.js
+++ example/cbus.js
@@ -4,20 +4,22 @@ const EventHub = require('../lib/events/EventBus');
 
 const Event = require('../lib/events/Event');
 const Command = require('../lib/commands/Command');
+const BackgroundCommand = require('../lib/commands/BackgroundCommand');
+
 const CommandBus = require('../lib/commands/CommandBus');
 const EventTransport = require('../lib/transports/emitter');
 const MqttTransport = require('../lib/transports/mqtt');
 const TransportManager = require('../lib/transports/transportManager');
 
-class AppRunCommand extends Command{
+class AppRunCommand extends Command {
     execute(event){
-        console.log('AppRunCommand: %j', event);
+        console.log('[%s] %s AppRunCommand:   \t', getTime(), event.uid, event.type);
     }
 }
 
-class PostRunCommand extends Command{
+class PrePostCommand extends Command {
     execute(event){
-        console.log('PostRunCommand: %j', event);
+        console.log('[%s] %s PrePostCommand:  \t', getTime(), event.uid, event.type);
     }
 }
 
@@ -35,4 +37,19 @@ let command = new CommandBus({
 });
 
 command.add('app.run', AppRunCommand);
-command.add('app.run.+', PostRunCommand);
+
+command.add('app.run', new BackgroundCommand({
+    commandsPath: './example',
+    runner: 'runner.js'
+}));
+
+command.add('app.run', function FunctionCommand(event){
+    console.log('[%s] %s FunctionCommand: \t', getTime(), event.uid, event.type);
+});
+
+
+command.add('app.run.+', PrePostCommand);
+
+function getTime(){
+    return new Date().toLocaleTimeString();
+}
